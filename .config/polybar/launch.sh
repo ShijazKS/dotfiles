7e40c50075abed6a68978f0dbfff9b89c2b843d7
ShijazKs <hafizshijaz@gmail.com>
2023-08-16 22:13:28 +0000

    initial update

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+DIR="$HOME/.config/polybar"
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+for i in /sys/class/hwmon/hwmon*/temp*_input; do 
+    if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "coretemp: Core 0" ]; then
+        export HWMON_PATH="$i"
+    fi
+done
+
+# Launch the bar
+polybar -q main -c "$DIR"/config.ini &
+polybar -q main2 -c "$DIR"/config.ini &
